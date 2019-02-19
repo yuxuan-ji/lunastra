353a9686666bea3e34dd6e395a1b8c2b0d049899
Y.J <yuxuan98@hotmail.com>
2019-02-18 20:42:22 -0500

    Initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+/* global __dirname, require, module*/
+
+const webpack = require('webpack');
+const path = require('path');
+const env = require('yargs').argv.env; // use --env with webpack 2
+const pkg = require('./package.json');
+
+let libraryName = 'lunastra';
+
+let outputFile, mode, devtool;
+
+if (env === 'build') {
+  mode = 'production';
+  outputFile = libraryName + '.min.js';
+  devtool = undefined;
+} else {
+  mode = 'development';
+  outputFile = libraryName + '.js';
+  devtool = 'source-map';
+}
+
+const config = {
+  mode: mode,
+  entry: __dirname + '/src/lunastra.js',
+  devtool: devtool,
+  output: {
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: "typeof self !== 'undefined' ? self : this"
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  }
+};
+
+module.exports = config;
