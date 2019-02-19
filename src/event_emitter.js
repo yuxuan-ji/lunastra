709c57ef9a901fd135f70f0bf3c9de1bf5cb6d86
Y.J <yuxuan98@hotmail.com>
2019-02-18 22:21:02 -0500

    Add f to add/remove listeners

--- src/event_emitter.js
+++ src/event_emitter.js
@@ -7,4 +7,28 @@ export class EventEmitter {
   hasHandler(name) {
     return name in this.events;
   };
+
+  addListener() {
+    var args = Array.prototype.slice.call(arguments);
+    var f = args.pop();
+    var names = args;
+
+    if (typeof f !== "function") throw new TypeError("Last argument must be a function");
+
+    names.forEach(function (name) {
+      if (!this.hasHandler(name)) this.events[name] = [];
+      this.events[name].push(f);
+    }, this);
+  }
+
+  removeListener(name, f) {
+    if (!this.hasHandler(name)) return;
+
+    var fIndex = this.events[name].indexOf(f);
+    if (fIndex === -1) return;
+
+    this.events[name].splice(fIndex, 1);
+
+    if (this.events[name].length === 0) delete this.events[name];
+  }
 }
