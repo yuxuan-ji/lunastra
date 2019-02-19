dc155723428609254dba86e93569f0b36e0561ce
Y.J <yuxuan98@hotmail.com>
2019-02-18 22:04:38 -0500

    Add f to add/remove documents

--- src/document_store.js
+++ src/document_store.js
@@ -1,22 +1,39 @@
+import {clone} from './utils.js';
+
 export class DocumentStore {
 
-  constructor(save = true) {
+  constructor(save = true, deepcpy = true) {
     this._save = (save !== false);
+    this._deepcpy = (deepcpy !== false);
     this.docs = {};
     this.docInfo = {};
     this.length = 0;
   }
 
-  get save() {
-    return this._save;
-  }
+  get save() {return this._save;}
+  get deepcpy() {return this._deepcpy;}
 
-  hasDoc(docRef) {
+  has(docRef) {
     return docRef in this.docs;
   }
 
-  getDoc(docRef) {
-    if (this.hasDoc(docRef) === false) return null;
+  get(docRef) {
+    if (this.has(docRef) === false) return null;
     return this.docs[docRef];
+  }
+
+  add(docRef, doc) {
+    if (!this.has(docRef)) this.length++;
+
+    this.docs[docRef] = this._save ? (this.deepcpy ? clone(doc) : doc) : null;
+
+  }
+
+  remove(docRef) {
+    if (!this.has(docRef)) return;
+
+    delete this.docs[docRef];
+    delete this.docsInfo[docRef];
+    this.length--;
   }
 }
