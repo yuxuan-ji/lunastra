8fce4ec6413161ef97d7fffda45466e2c4c5827e
Y.J <yuxuan98@hotmail.com>
2019-02-18 22:22:36 -0500

    Add f to emit a listener

--- src/event_emitter.js
+++ src/event_emitter.js
@@ -31,4 +31,14 @@ export class EventEmitter {
 
     if (this.events[name].length === 0) delete this.events[name];
   }
+
+  emit(name) {
+    if (!this.hasHandler(name)) return;
+
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    this.events[name].forEach(function (f) {
+      f.apply(undefined, args);
+    }, this);
+  }
 }
