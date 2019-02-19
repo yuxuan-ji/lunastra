9389f6a6fb6b7328e35a1b2577c4aaddda4f50f2
Y.J <yuxuan98@hotmail.com>
2019-02-18 21:11:11 -0500

    Add method to retrieve a registered f

--- src/pipeline.js
+++ src/pipeline.js
@@ -1,8 +1,24 @@
+import {warn} from './utils.js';
+
 export class Pipeline {
 
   constructor() {
     this.__queue = [];
   }
 
+  static registerFunction(f, label) {
+    if (label in Pipeline.registeredFunctions) {
+      warn('Overwriting existing registered function: ' + label);
+    }
+
+    f.label = label;
+    Pipeline.registeredFunctions[label] = f;
+  }
+
+  static getRegisteredFunction(label) {
+    if (!(label in Pipeline.registeredFunctions)) return null;
+    return Pipeline.registeredFunctions[label];
+  }
 }
 
+Pipeline.registeredFunctions = {};
