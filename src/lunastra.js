90c0c5998ff2f41b0856cfc632c66b106bc0d7b9
Y.J <yuxuan98@hotmail.com>
2019-02-21 16:25:17 -0500

    Complete base lunastra startup file

--- src/lunastra.js
+++ src/lunastra.js
@@ -1,7 +1,16 @@
 import {Index} from './index.js';
+import {Trimmer} from './trimmer.js';
+import {StopWordFilter} from './stop_word_filters.js';
+import {Stemmer} from './stemmer.js';
 
 export function init(config) {
   var index = new Index();
+
+  index.pipeline.add(
+    Trimmer.trim,
+    StopWordFilter.filter,
+    Stemmer.stem
+  );
 
   if (config) config.call(index, index);
 
