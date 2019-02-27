fd69942c510c3c5df5ac04c89722a092c8daef31
Y.J <yuxuan98@hotmail.com>
2019-02-27 09:40:44 -0500

    Add docs for trimmer

--- src/trimmer.js
+++ src/trimmer.js
@@ -1,5 +1,14 @@
+/**
+ * Pipeline Extension: Trimmer
+ */
 export class Trimmer {
 
+  /**
+   * Trim non word characters from the begining and end of a string
+   * and return the result
+   * @param  {string} token
+   * @return {string}
+   */
   static trim(token) {
     if (token === null || token === undefined) {
       throw new Error('token should not be undefined');
