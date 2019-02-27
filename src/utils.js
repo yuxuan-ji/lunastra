178f5c364c4b95306f6a874d30f3d95abd809579
Y.J <yuxuan98@hotmail.com>
2019-02-27 09:41:18 -0500

    Add docs for utils

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
+/**
+ * Print a warning message to the console
+ * @param  {string} message
+ */
 export function warn(global) {
   return function (message) {
     if (global.console && console.warn) {
@@ -6,12 +10,24 @@ export function warn(global) {
   };
 }
 
+/**
+ * Returns and empty string for null and undefined,
+ * otherwise returns the result of the object's
+ * '.toString()' method
+ * @param  {object} obj
+ * @return {string}
+ */
 export function toString(obj) {
   if (obj === undefined || obj === null) return "";
 
   return obj.toString();
 }
 
+/**
+ * Return a deep copy of the object
+ * @param  {object} obj
+ * @return {object}
+ */
 export function clone(obj) {
   if (obj === null || typeof obj !== "object") return obj;
   var copy = obj.constructor();
