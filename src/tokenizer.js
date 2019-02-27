d20e1ad60fe4f119b6d04e9b2ed9c1fc04d5804a
Y.J <yuxuan98@hotmail.com>
2019-02-27 09:33:44 -0500

    Add docs for tokenizer

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -2,6 +2,11 @@ import {toString} from './utils.js';
 
 export class Tokenizer {
 
+  /**
+   * Split a string into a list of tokens
+   * @param  {string} str
+   * @return {string[]}
+   */
   static tokenize(str) {
     if (!arguments.length || str === null || str === undefined) return [];
     if (Array.isArray(str)) {
@@ -29,14 +34,25 @@ export class Tokenizer {
     return str.toString().trim().toLowerCase().split(Tokenizer.separator);
   }
 
+  /**
+   * Get the seperator
+   * @return {string|regexp}
+   */
   static getSeparator() {
     return Tokenizer.separator;
   }
 
+  /**
+   * Set the seperator
+   * @param {string|regexp} sep
+   */
   static setSeparator(sep) {
     Tokenizer.separator = sep;
   }
 
+  /**
+   * Reset to the default seperator
+   */
   static resetSeparator() {
     Tokenizer.separator = Tokenizer.defaultSeparator;
   }
