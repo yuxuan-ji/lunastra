34107c4a0a0b82a79edcd4666eef1640ab587b68
Y.J <yuxuan98@hotmail.com>
2019-02-27 09:27:19 -0500

    Add docs for pipeline

--- src/pipeline.js
+++ src/pipeline.js
@@ -1,21 +1,39 @@
+/**
+ * A pipeline is an ordered list of functions applied to both document
+ * tokens and query tokens.
+ */
 export class Pipeline {
 
+  /**
+   * Initialize a pipeline
+   */
   constructor() {
     this._queue = [];
   }
 
+  /**
+   * Return the stored list of functions
+   * @return {function[]}
+   */
   get() {
     return this._queue;
   }
 
-  add() {
-    var fs = Array.prototype.slice.call(arguments);
-
+  /**
+   * Push functions to the end of the pipeline
+   * @param {...function} fs
+   */
+  add(...fs) {
     fs.forEach(function (f) {
       this._queue.push(f);
     }, this);
   }
 
+  /**
+   * Insert a function after a function present in the pipeline
+   * @param  {function} existingFct
+   * @param  {function} newFct
+   */
   after(existingFct, newFct) {
     var pos = this._queue.indexOf(existingFct);
     if (pos === -1) throw new Error('Cannot find the the requested function to insert after');
@@ -23,6 +41,11 @@ export class Pipeline {
     this._queue.splice(pos + 1, 0, newFct);
   };
 
+  /**
+   * Insert a function before a function present in the pipeline
+   * @param  {function} existingFct
+   * @param  {function} newFct
+   */
   before(existingFct, newFct) {
     var pos = this._queue.indexOf(existingFct);
     if (pos === -1) throw new Error('Cannot find the the requested function to insert after');
@@ -30,6 +53,10 @@ export class Pipeline {
     this._queue.splice(pos, 0, newFct);
   };
 
+  /**
+   * Remove a function from the pipeline
+   * @param  {function} f
+   */
   remove(f) {
     var pos = this._queue.indexOf(f);
     if (pos === -1) return;
@@ -37,17 +64,30 @@ export class Pipeline {
     this._queue.splice(pos, 1);
   };
 
+  /**
+   * Remove and return the last function in the pipeline
+   * @return {function}
+   */
   pop() {
-    this._queue.pop();
+    return this._queue.pop();
   }
 
+  /**
+   * Empty the pipeline
+   */
   clear() {
     this._queue = [];
   }
 
+  /**
+   * Run each function stored in the pipeline on the input tokens
+   * in FIFO order and returns the result
+   * @param  {string[]} tokens
+   * @return {string[]}
+   */
   run(tokens) {
-    var out = tokens,
-      queueLength = this._queue.length;
+    var out = tokens;
+    var queueLength = this._queue.length;
     for (var i = 0; i < queueLength; i++) {
       var pipelineOutput = [];
       for (let j = 0; j < out.length; j++) {
