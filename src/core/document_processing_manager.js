11ebd3574f0fd320b7f6d8b0534d552b609ca02b
Y.J <yuxuan98@hotmail.com>
2019-03-01 16:55:05 -0500

    Add template for dpm

---
+++ src/core/document_processing_manager.js
@@ -0,0 +1,29 @@
+import {Pipeline} from './pipeline.js';
+
+export class DocumentProcessingManager {
+  constructor() {
+    this._preconversion = new Pipeline();
+    this._postconversion = new Pipeline();
+  }
+
+  processing() {
+
+  }
+
+  mapping() {
+
+  }
+
+  addPreExtension() {
+
+  }
+
+  addPostExtension() {
+
+  }
+
+  run() {
+
+  }
+
+}
