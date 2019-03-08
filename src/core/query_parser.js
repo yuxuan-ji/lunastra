17ecd66907204ff816eea3fe4a0d681e839f432b
Y.J <yuxuan98@hotmail.com>
2019-03-08 10:07:14 -0500

    Add base query parser

---
+++ src/core/query_parser.js
@@ -0,0 +1,20 @@
+/**
+ * Utility class to convert a user query into a model understandable
+ * by the Lunastra Index
+ */
+export class QueryParser {
+
+  /**
+   * Build the query into a model understandable by the Lunastra Index
+   * @param  {string} query
+   * @return {object} model
+   */
+  static build(query) {
+    var model = {
+      q: "",
+      config: {}
+    };
+    return model;
+  }
+
+}
