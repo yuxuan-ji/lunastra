c28abebcc5c51cbaff3d96086db340d437ee2b2e
Y.J <yuxuan98@hotmail.com>
2019-04-11 16:15:25 -0400

    Make init accept an object

--- src/lunastra.js
+++ src/lunastra.js
@@ -5,7 +5,11 @@ import {Stemmer} from './exts/stemmer.js';
 
 /**
  * A helper method to initialize a Lunastra Index
- * @param  {Function} config
+ * @param  {Object} config
+ * @param {String} config.id field used to uniquely identify a document (default is 'id')
+ * @param {Boolean} config.save whether to save documents in the document store (default is true)
+ * @param {String[]} config.fields fields to be registered in the index
+ * @param {Object[]} config.documents documents to add to the index
  * @return {Index}
  */
 export function init(config) {
@@ -17,7 +21,14 @@ export function init(config) {
     Stemmer.stemmer
   );
 
-  if (config) config.call(index, index);
+  if (config.id) index.setId(config.id);
+  if (config.save === false) index.saveDoc(false);
+  if (config.fields && config.fields.length > 0) {
+    config.fields.forEach(function (field) {index.addField(field);});
+  }
+  if (config.documents && config.documents.length > 0) {
+    config.documents.forEach(function (doc) {index.addDoc(doc);});
+  }
 
   return index;
 }
